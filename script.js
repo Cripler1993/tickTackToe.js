f4a4abff932b237fcfec3c4f40211408b83ff5ec
Cripler1993 <cripler1993@mail.ru>
2023-12-15 11:25:22 +0300

    new commit

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ const winPosition = [
   [1, 5, 9],
   [3, 5, 7],
 ];
+let playerName1 = document.querySelector("#player__name-1");
+let playerName2 = document.querySelector("#player__name-2");
+let playerScore1 = document.querySelector("#player__score-1");
+let playerScore2 = document.querySelector("#player__score-2");
+let drawScore = document.querySelector("#draw__score");
+let restartBtn = document.querySelector(".restart__btn");
+
 let modalwrapper = document.querySelector(".modal__wrapper");
+let winner = document.querySelector(".winner__name");
+let closeBtn = document.querySelector(".modal__btn");
+
+let player1 = prompt("Введите имя первого игрока?");
+let player2 = prompt("Введите имя второго игрока?");
+playerName1.innerHTML = player1;
+playerName2.innerHTML = player2;
 let currentPlayer = 1; //1 - x; 2 - o
 let gameEnd = false;
 
@@ -26,6 +40,13 @@ cards.forEach(function (elem) {
   elem.addEventListener("click", makeMove);
 });
 
+closeBtn.addEventListener("click", function () {
+  modalwrapper.classList.remove("active");
+  restartBtn.classList.add("active");
+});
+
+restartBtn.addEventListener("click", restartGame);
+
 //локальную переменную card определяем как цель клика
 // определяем переменную container как элемент back из html
 // если переменная контейнер пустая
@@ -40,7 +61,7 @@ cards.forEach(function (elem) {
 function makeMove(event) {
   let card = event.target;
   let container = card.querySelector(".back");
-  if (!container.innerHTML) {
+  if (!container.innerHTML && !gameEnd) {
     if (currentPlayer == 1) {
       container.innerHTML = cross;
       checkWin();
@@ -54,7 +75,25 @@ function makeMove(event) {
   }
 }
 
+// объявляем функцию checkWin
+// проходимся по массиву с помощью цикла for
+// в переменную winOption присваиваем каждый элемент массива winPosition
+// в переменные id 1, 2, 3 присваиваем цифры содержащиеся в подмассивах winOption
+//
+// в переменные back1, 2, 3 присваиваем элементы с классом back
+// если внутренний html переменных back 1, 2, 3 равен cross, то запускаем функцию youWin и прерываем цикл
+// для circle то же самое
 function checkWin() {
+  // const winPosition = [
+  //   [1, 2, 3],
+  //   [4, 5, 6],
+  //   [7, 8, 9],
+  //   [1, 4, 7],
+  //   [2, 5, 8],
+  //   [3, 6, 9],
+  //   [1, 5, 9],
+  //   [3, 5, 7],
+  // ];
   for (let i = 0; i < winPosition.length; i++) {
     let winOption = winPosition[i];
     let id1 = winOption[0];
@@ -83,27 +122,31 @@ function checkWin() {
     }
   }
 }
-
-function youWin() {
-  let winner = document.querySelector(".winner__name");
-  if (currentPlayer == 1) {
-    winner.innerHTML = "крестиками";
-  } else {
-    winner.innerHTML = "ноликами";
-  }
-  modalwrapper.classList.add("active");
-  modalClose();
-}
-
 // winPosition.forEach(function(winOption){
 
 // })
 
-function modalClose() {
-  let closeBtn = document.querySelector(".modal__btn");
-  closeBtn.addEventListener("click", function () {
-    modalwrapper.classList.remove("active");
+function youWin() {
+  if (currentPlayer == 1) {
+    winner.innerHTML = player1;
+    playerScore1.innerHTML = Number(playerScore1.innerHTML) + 1;
+  } else {
+    winner.innerHTML = player2;
+    playerScore2.innerHTML = Number(playerScore2.innerHTML) + 1;
+  }
+  modalwrapper.classList.add("active");
+  gameEnd = true;
+}
+
+function restartGame() {
+  currentPlayer = 1;
+  gameEnd = false;
+  cards.forEach(function (elem) {
+    elem.classList.remove("flip");
+    let container = elem.querySelector(".back");
+    container.innerHTML = "";
   });
+  restartBtn.classList.remove("active");
 }
 
 // анимация победы
